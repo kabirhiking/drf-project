f5a9e71a5388f5d289329728366c0eb30991b27b
kabir <kabirraihan249@gmail.com>
2025-07-13 17:02:59 +0600

    create serialization

--- api/views.py
+++ api/views.py
@@ -1,12 +1,27 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from api.serializers import StudentSerializer
 from students.models import Student
 
-# Create your views here.
 
+# DRF serializ
+@api_view(['GET'])
 def studentsView(request):
-    students = Student.objects.all()
-    students_list = list(students.values()) 
-    # object converted to JSON serializable
-     
-    return JsonResponse(students_list, safe=False)
+    if request.method == 'GET':
+        # get all the data from the student table
+        Students = Student.objects.all()
+        serializer = StudentSerializer(Students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+# manual serializers
+# def studentsView(request):
+#     students = Student.objects.all()
+#     students_list = list(students.values()) 
+#     # object or query set converted to JSON format (serializable)
+#     return JsonResponse(students_list, safe=False)
+
+
+# Complex data --> Serializers --> JSON(XML)
