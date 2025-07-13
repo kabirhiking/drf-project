490fdbbe8105407b33e195b5f23072adfbac2e86
kabir <kabirraihan249@gmail.com>
2025-07-14 00:11:49 +0600

    getting single object

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('students/', views.studentsView),
     path('students/<int:pk>/', views.studentDetailView),
-    path('employees/', views.Employees.as_view())
+    
+    path('employees/', views.Employees.as_view()),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view())
     
     
 ]
--- api/views.py
+++ api/views.py
@@ -5,9 +5,11 @@ from api.serializers import EmployeeSerializer, StudentSerializer
 from students.models import Student
 from rest_framework.views import APIView
 from employees.models import Employee
+from django.http import Http404
 
 
 # DRF serializatio
+# function based serializer
 @api_view(['GET', 'POST'])
 def studentsView(request):
     if request.method == 'GET':
@@ -58,10 +60,30 @@ def studentDetailView(requst, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
 
+
+# class based serializer
 class Employees(APIView):
     def get(self, request):
         empployees = Employee.objects.all()
         serializer = EmployeeSerializer(empployees, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
         
-        
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+       
+
+class EmployeeDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+            
+    def get(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)        
