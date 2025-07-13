336fd684a4c2488ce8e823afc1fd4f884c2c2a28
kabir <kabirraihan249@gmail.com>
2025-07-13 15:23:52 +0600

    create manual Serialization

--- api/views.py
+++ api/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from students.models import Student
 
 # Create your views here.
 
 def studentsView(request):
-    students = {
-        'id' : 1,
-        'name' : 'kabir',
-        'class' : "BSC"
-    }
-    
-    return JsonResponse(students)
+    students = Student.objects.all()
+    students_list = list(students.values()) 
+    # object converted to JSON serializable
+     
+    return JsonResponse(students_list, safe=False)
