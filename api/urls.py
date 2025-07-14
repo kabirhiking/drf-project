81b82924d9fea06e00a73c2982139e962a476282
kabir <kabirraihan249@gmail.com>
2025-07-14 15:11:30 +0600

    update and delete

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # function based url
     path('students/', views.studentsView),
     path('students/<int:pk>/', views.studentDetailView),
-    
+    # class based url
     path('employees/', views.Employees.as_view()),
     path('employees/<int:pk>/', views.EmployeeDetail.as_view())
     
--- api/views.py
+++ api/views.py
@@ -86,4 +86,18 @@ class EmployeeDetail(APIView):
     def get(self, request, pk):
         employee = self.get_object(pk)
         serializer = EmployeeSerializer(employee)
-        return Response(serializer.data, status=status.HTTP_200_OK)        
+        return Response(serializer.data, status=status.HTTP_200_OK)  
+    
+    def put(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+         
+    def delete(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
